8f8cd7c257ff84c93e7f6cd5466f5f686121991c
unknown <anilkumarravuri510@gmail.com>
2023-05-06 14:12:21 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -1,12 +1,13 @@
+# THIS IS GOING TO INJECT THE SCHEMA OF MYSQL
 resource "null_resource" "schema" {
   provisioner "local-exec" {
-    command = <<EOF
-cd/tmp
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-unzip mysql.zip
-cd mysql-main
-mysql -h  ${aws_db_instance.mysql.address}  -uadmin1  -pRoboShop1 < shipping.sql
+  command = <<EOF
+       cd /tmp
+       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+       unzip mysql.zip
+       cd mysql-main
+       mysql -h  ${aws_db_instance.mysql.address}  -uadmin1  -pRoboShop1 < shipping.sql
 
-EOF
+    EOF
   }
 }
