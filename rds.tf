d8e3fdccbab29d6b5ec7f479ab3dd39d50e80220
unknown <anilkumarravuri510@gmail.com>
2023-05-06 13:08:42 +0530

    auto push

--- rds.tf
+++ rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "mysql" {
   instance_class           = "db.t3.micro"
   username                 = "admin1"
   password                 = "RoboShop1"
-  parameter_group_name     = "default.mysql5.7"
+  parameter_group_name     = aws_db_parameter_group.mysql_pg.name
   skip_final_snapshot      = true    # this will ensure you to take screenshot when you destroy
   db_subnet_group_name     =  aws_db_subnet_group.mysql_subnet_group.name
   vpc_security_group_ids   = [aws_security_group.allow_mysql.id]
