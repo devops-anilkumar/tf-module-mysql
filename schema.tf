fcead9de04444e457cc3a822d32e147cf568e623
unknown <anilkumarravuri510@gmail.com>
2023-05-17 00:27:20 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -5,11 +5,10 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
   command = <<EOF
        cd /tmp
-       rm -rf /tmp/mysql-main
        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
        unzip mysql.zip
        cd mysql-main
-       mysql -h  ${aws_db_instance.mysql.address}  -uadmin1  -pRoboShop1 < shipping.sql
+       mysql -h  ${aws_db_instance.mysql.address}  -uRDS_USER  -pRDS_PASSWORD < shipping.sql
 
     EOF
   }
