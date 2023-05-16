cff239169871537f9694f4ebd5a4bfcd7c66d754
unknown <anilkumarravuri510@gmail.com>
2023-05-17 00:09:58 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -5,6 +5,7 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
   command = <<EOF
        cd /tmp
+       rm -rf /tmp/mysql-main
        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
        unzip mysql.zip
        cd mysql-main
