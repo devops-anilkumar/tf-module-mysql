ea6123b9f12ebe520008035ea06a347a0834c265
unknown <anilkumarravuri510@gmail.com>
2023-05-17 01:10:45 +0530

    auto push

--- schema.tf
+++ schema.tf
@@ -8,7 +8,7 @@ resource "null_resource" "schema" {
        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
        unzip -o mysql.zip
        cd mysql-main
-       mysql -h  ${aws_db_instance.mysql.address}  -uRDS_USER  -pRDS_PASSWORD < shipping.sql
+       mysql -h  ${aws_db_instance.mysql.address}  -u${RDS_USER}  -p${RDS_PASSWORD} < shipping.sql
 
     EOF
   }
